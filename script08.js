109fa08fec676ab27a27216a1e5f6a783ebb0782
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-16 17:23:19 +0800

    / while迴圈

---
+++ script08.js
@@ -0,0 +1,15 @@
+// while迴圈
+var i = 1
+
+while (i <= 3) {
+  document.write(i)
+  document.write('<br>')
+  i ++
+}
+
+
+do{
+  document.write(i)
+  document.write('<br>')
+  i ++
+}while (i <= 7) //先做再判斷
