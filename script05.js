558ffa87dfa3167221bbc19733a8347a25f2eab0
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-16 16:31:53 +0800

    Update script05.js

--- script05.js
+++ script05.js
@@ -1,5 +1,16 @@
 //function 函式
 
-function hello(){
-    document.write("你好")
+function hello(name, age) {
+  //function
+  document.write('你好' + name + '你今年' + age + '歲')
 }
+
+hello('小白', 28) //呼叫
+
+document.write('<br>')
+
+function add(num1, num2) {
+  //document.write(num1+num2);
+  return num1 + num2
+}
+document.write(add(10, 20))
