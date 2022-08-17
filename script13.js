04debdb527b64009952ede8169efd4ccc0a26e47
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-17 15:40:31 +0800

    class

--- script13.js
+++ script13.js
@@ -1,1 +1,44 @@
-//  class 
+//  class
+class Phone {
+  constructor(number, year, is_waterproof) {
+    this.number = number
+    this.year = year
+    this.is_waterproof = is_waterproof
+  }
+  phone_age() {
+    return 2021 - this.year
+  }
+}
+
+var phone1=new Phone("123",2020, false)
+var phone2=new Phone("456",2018, false)
+document.write(phone1.phone_age())
+document.write("<br>")
+document.write(phone2.phone_age())
+
+/* var phone = {
+  number: '123',
+  year: '2020',
+  is_waterproof: 'false',
+  phone_age: function () {
+    return 2021 - this.year
+  },
+}
+
+var phone = {
+  number: '456',
+  year: '2018',
+  is_waterproof: 'false',
+  phone_age: function () {
+    return 2021 - this.year
+  },
+}
+
+var phone = {
+  number: '789',
+  year: '2018',
+  is_waterproof: 'true',
+  phone_age: function () {
+    return 2021 - this.year
+  }, 
+}*/
