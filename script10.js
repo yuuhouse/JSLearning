204c5f009e414f36cf7567d4b23bcdbbd50a93b2
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-17 15:10:33 +0800

    for 迴圈

---
+++ script10.js
@@ -0,0 +1,12 @@
+// for 迴圈
+
+for(var i=0;i<=10;i++){
+  document.write("x")
+  document.write("<br>")
+}
+
+var friends=["小黑",'小白','小綠']
+for( var i=0; i<friends.length; i++){
+  document.write(friends[i])
+  document.write('<br>')
+}
