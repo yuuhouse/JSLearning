3591e40bd0ae1e7ee2b81e082a88550de610d03b
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-20 18:01:03 +0800

    如何取得html元素

--- script14.js
+++ script14.js
@@ -1,6 +1,14 @@
 // 如何取得html元素
-document.write("哈哈哈");
-window.document.write("哈哈哈");
-var a = 123;
+document.write('哈哈哈')
+window.document.write('哈哈哈')
+var a = 123
 window.document.write(window.a)
 /* window.open() */
+var h1 = document.getElementById('header')
+console.log(h1)
+h1.innerText = 'Hello, world!'
+h1.style.backgroundColor = 'red'
+h1.style.color = 'green'
+var link = document.getElementById('link')
+link.href = 'http://amazon.com'
+console.log(link)
