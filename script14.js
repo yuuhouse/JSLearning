918c94661a7edf7e65f652e0108ad3844c5e33aa
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-20 17:45:16 +0800

    Update script14.js

--- script14.js
+++ script14.js
@@ -1,44 +1,6 @@
-//  class
-class Phone {
-  constructor(number, year, is_waterproof) {
-    this.number = number
-    this.year = year
-    this.is_waterproof = is_waterproof
-  }
-  phone_age() {
-    return 2021 - this.year
-  }
-}
-
-var phone1=new Phone("123",2020, false)
-var phone2=new Phone("456",2018, false)
-document.write(phone1.phone_age())
-document.write("<br>")
-document.write(phone2.phone_age())
-
-/* var phone = {
-  number: '123',
-  year: '2020',
-  is_waterproof: 'false',
-  phone_age: function () {
-    return 2021 - this.year
-  },
-}
-
-var phone = {
-  number: '456',
-  year: '2018',
-  is_waterproof: 'false',
-  phone_age: function () {
-    return 2021 - this.year
-  },
-}
-
-var phone = {
-  number: '789',
-  year: '2018',
-  is_waterproof: 'true',
-  phone_age: function () {
-    return 2021 - this.year
-  }, 
-}*/
+// 如何取得html元素
+document.write("哈哈哈");
+window.document.write("哈哈哈");
+var a = 123;
+window.document.write(window.a)
+/* window.open() */
