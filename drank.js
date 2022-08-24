97da3b6e4e5426a5992a43d6d306c50aa5a2a1ed
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-24 23:00:02 +0800

    delete

---
+++ drank.js
@@ -0,0 +1,13 @@
+/* var drank = false
+console.log('Is it too salty?')
+drank = true
+while (drank == true) {
+  setTimeout(function () {
+    console.log('延遲')
+  }, 5000)
+  drank = false
+  console.log('嚐嚐看看')
+  console.log('會很鹹嗎')
+  drank = true
+  console.log('喝一口')
+} */
