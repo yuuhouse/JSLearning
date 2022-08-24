885141d30a9ae6c7b272f65099ac527fb6bd2efb
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-24 23:40:59 +0800

    facoriteFood

--- script17.js
+++ script17.js
@@ -1,13 +1,21 @@
+//解構物件 //Array_Object_Destructuring
 const person = {
-  name: '小白',
+  myName: '小白',
   age: 26,
-  favoriteFood: 'WaterMelon' ,
+  favoriteFood: 'WaterMelon',
 }
-console.log(person.name)
+/* console.log(person.name) */
 
-const {name} = person
-console.log(name)
+const { name } = person
+/* console.log(name) */
 
-const people=['小明','杰倫','漂亮阿姨']
-const[Ming,Jay,Auntie]=people
-console.log(Ming,Jay,Auntie)
+const people = ['小明', '杰倫', '漂亮阿姨']
+/* const [Ming, Jay, Auntie] = people
+console.log(Ming, Jay, Auntie)
+ */
+//重新命名
+const{myName:newName}=person;
+console.log(newName)
+
+const {favoriteFood}=person
+console.log(favoriteFood)
