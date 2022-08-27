bcc36a3f6c5522b73116b05f1ab51990903fa007
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-08-27 16:38:18 +0800

    fetch api https://randomuser.me/api/

--- script17.js
+++ script17.js
@@ -14,8 +14,29 @@ const people = ['小明', '杰倫', '漂亮阿姨']
 console.log(Ming, Jay, Auntie)
  */
 //重新命名
-const{myName:newName}=person;
-console.log(newName)
+const { myName: newName } = person
+// console.log(newName)
 
-const {favoriteFood}=person
-console.log(favoriteFood)
+const { favoriteFood = 'Ramen' } = person //預設值
+//console.log(favoriteFood)
+
+fetch('https://randomuser.me/api/')
+  .then((res) => {
+    return res.json()
+  })
+  .then((json) => {
+    /* console.log(json.results[0]) */
+    const [result] = json.results
+    //console.log(result)
+    const {email, phone} = result;
+    //console.log(email,phone)//取得email, phone
+  })
+
+//Vue, React > Props
+/* function fn(Props){
+  console.log(Props);
+} */
+function fn({myName, favoriteFood}){  //加入大括號與不加 顯示結果有差
+  console.log(myName, favoriteFood);
+}
+fn(person)
