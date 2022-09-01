56cf7072d0a77844346ed91f3131da1c7889a1b9
yuuhouse <44646567+yuuhouse@users.noreply.github.com>
2022-09-01 20:47:44 +0800

    JavaScript Classes

---
+++ script20.js
@@ -0,0 +1,13 @@
+//The new Map() Method
+document.getElementById("abcd").innerHTML=""
+
+//JavaScript Classes
+class ClassName {
+    constructor(name,year){
+        this.name=name;
+        this.year=year;
+    }
+}
+
+const myCar1=new ClassName("Ford",2014);
+const myCar2=new ClassName("Audi",2019);
